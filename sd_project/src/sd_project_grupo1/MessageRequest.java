4c433ea53369ccbc986d8832d3ca521e33a6340f
guettuh <a86222@alunos.uminho.pt>
2019-11-12 00:03:00 +0000

    mais limpo

--- sd_project/src/sd_project_grupo1/MessageRequest.java
+++ sd_project/src/sd_project_grupo1/MessageRequest.java
@@ -1,19 +1,15 @@
-package sd_project_grupo1; 
+package sd_project_grupo1;
 // hashtable para guardar os pedidos (Mensagem) e as pedidos
+
 // mapeia chaves para valores. Qualquer objeto não nulo pode ser usado como uma
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
+import java.util.*;
 
 // chave ou como um valor.
 
 public class MessageRequest {
-    private static final HashMap<String, Message> messageshash = new HashMap<String, Message>(); 
-    private static final HashMap<String,User> listUsers = new HashMap<String,User>();
+    private static final HashMap<String, Message> messageshash = new HashMap<String, Message>();
+    private static final HashMap<String, User> listUsers = new HashMap<String, User>();
     private static final int iDMessage = 1;
     private User users;
     private static int iDRegister;
@@ -46,120 +42,113 @@ public class MessageRequest {
     public int getIDMensagem() {
         return IDMensagem;
     }
-    
-    
-    
-    
-    public static int getIDMessage(){
+
+    public static int getIDMessage() {
         return iDMessage;
     }
+
     private int IDMensagem;
-    
-    
-    
-    public String printUsers (){
-		String result=new String("");
-		for (String name: listUsers.keySet()){
-                    String key = name.toString();
-                    String value = listUsers.get(name).getNickName().toString();
-                    result += key + " - " + value;
-                    result += "<br>";
-                    }
-			
-		
-		return result;
-	}
-    
-    
-    //método para guardar as mensagens
-    //syncronized para não ser mais que uma thread a aceder ao código dentro desse bloco
-    public void saveUser(User newUser){
-        if(!listUsers.containsValue(newUser)){
+
+    public String printUsers() {
+        String result = new String("");
+        for (String name : listUsers.keySet()) {
+            String key = name.toString();
+            String value = listUsers.get(name).getNickName().toString();
+            result += key + " - " + value;
+            result += "<br>";
+        }
+
+        return result;
+    }
+
+    // método para guardar as mensagens
+    // syncronized para não ser mais que uma thread a aceder ao código dentro desse
+    // bloco
+    public void saveUser(User newUser) {
+        if (!listUsers.containsValue(newUser)) {
             System.out.println("Não existe utilizador");
             User user = newUser;
             listUsers.put(String.valueOf(listUsers.size() + 1), user);
             System.out.println(listUsers.containsValue(user) + "Criado");
-        }else{
+        } else {
             System.out.println("Já existe utilizador");
         }
     }
-    public void saveMessage(User user, String message){
-        synchronized(this){
-            if(!listUsers.containsValue(user)){
+
+    public void saveMessage(User user, String message) {
+        synchronized (this) {
+            if (!listUsers.containsValue(user)) {
                 Message newMessage = new Message(user.getNickName());
                 newMessage.setMessage(message);
                 newMessage.setIDMessage(this.IDMensagem);
                 messageshash.put((String.valueOf(this.IDMensagem)), newMessage);
-                System.out.println("o hash tem "+messageshash.size()+ " ocupados");
-                IDMensagem+=1;
-            }else if(listUsers.containsValue(user)){
+                System.out.println("o hash tem " + messageshash.size() + " ocupados");
+                IDMensagem += 1;
+            } else if (listUsers.containsValue(user)) {
                 System.out.println("Registe-se Primeiro");
-                //Colocar pop-up
+                // Colocar pop-up
             }
         }
-        
+
     }
-    public String printMessages( ){
-    String result ="";
-    
-		for (String nickName: messageshash.keySet()){
-                    String key = nickName.toString();
-                    String user = messageshash.get(key).getNickName().toString();
-                    String value = messageshash.get(key).getMessage().toString().replace("+", " ");
-                    result += user + " : " + value;
-                    result += "<br>";
-                    }
-			
-		
-		return result;
-	}
-                
-    
-    class Message{
+
+    public String printMessages() {
+        String result = "";
+
+        for (String nickName : messageshash.keySet()) {
+            String key = nickName.toString();
+            String user = messageshash.get(key).getNickName().toString();
+            String value = messageshash.get(key).getMessage().toString().replace("+", " ");
+            result += user + " : " + value;
+            result += "<br>";
+        }
+
+        return result;
+    }
+
+    class Message {
         private String nickName;
         private String message;
         private int IDMessage;
-        
-        
-        public Message(String nickName, String message, int IDMessage){
+
+        public Message(String nickName, String message, int IDMessage) {
             this.nickName = nickName;
             this.message = message;
             this.IDMessage = iDMessage;
         }
 
-        public Message(String nickName){
+        public Message(String nickName) {
             this.nickName = nickName;
             this.IDMessage = 0;
         }
-       
-        public Message(){
+
+        public Message() {
             this.IDMessage = 0;
         }
-        
-        public int getIDMessage(){
+
+        public int getIDMessage() {
             return IDMessage;
         }
-    
 
         public void setMessage(String message) {
-            this.message= message;
+            this.message = message;
         }
 
         public void setIDMessage(int IDMessage) {
             this.IDMessage = IDMessage;
         }
-        
-        public String getNickName(){
+
+        public String getNickName() {
             return this.nickName;
         }
-        
-        public void setNickame(String nickName){
+
+        public void setNickame(String nickName) {
             this.nickName = nickName;
         }
-        
-        public String getMessage(){
+
+        public String getMessage() {
             return message;
         }
-    
+
     }
 }
--- sd_project/src/sd_project_grupo1/User.java
+++ sd_project/src/sd_project_grupo1/User.java
@@ -1,64 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sd_project_grupo1;
 
-import java.util.*;
-
-
 public class User {
-    private int IDRegisto;
     private String nickName;
-    private  HashMap<String, User> listUsers = new HashMap<String, User>();
-    
-   
 
     public User(String nickName) {
         this.nickName = nickName;
-        
-    }
 
-   public HashMap<String, User> getUsers() {
-    return   listUsers;
-   }
-   
-   public void saveUser(User newUser){
-        if(!listUsers.containsValue(newUser)){
-            System.out.println("Não existe utilizador");
-            User user = newUser;
-            listUsers.put(String.valueOf(listUsers.size() + 1), user);
-            System.out.println(listUsers.containsValue(user));
-        }else{
-            System.out.println("Já existe utilizador");
-        }
     }
- 
-   public ArrayList<String> getUsersString(){
-       ArrayList<String> listUsersString = new ArrayList<String>();
-       
-       for(int i=0; i<listUsers.size(); i++ ){
-            String user = listUsers.get(i).toString();
-            listUsersString.add(user);
-       }
-        return listUsersString;
-   }
-   
-
-    
 
     public void setNickName(String nickName) {
         this.nickName = nickName;
     }
 
-    
-
-    public int getIDRegisto(User user) {
-        return listUsers.get(user).hashCode();
-    }
-    
-    public String getNickName(){
+    public String getNickName() {
         return nickName;
     }
 }
